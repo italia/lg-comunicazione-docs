992594b6ede0afd3c899ec644c4b3d0828f30cd8
Davide Porrovecchio <pdavide@gmail.com>
2018-11-26 15:54:34 +0100

    Fix option value

--- conf.py
+++ conf.py
@@ -245,7 +245,7 @@ latex_logo = "media/agid-logo.png"
 latex_show_pagerefs = False
 
 # If true, show URL addresses after external links.
-latex_show_urls = False
+latex_show_urls = 'no'
 
 # Documents to append as an appendix to all manuals.
 #latex_appendices = []
